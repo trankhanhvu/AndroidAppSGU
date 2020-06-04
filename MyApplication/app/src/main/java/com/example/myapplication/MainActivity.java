88960720596e449a4281e515b64ab73550eb9837
Khanh Vu <khanhvuu1998@gmail.com>
2020-06-04 10:53:34 +0700

    fix crash app when not enter doC,doF

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,35 +6,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import vn.com.hotrotinhoc.convertnd.convert;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnCtoF,btnFtoC,btnClear;
+    Button btnCtoF, btnFtoC, btnClear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCtoF = (Button)findViewById(R.id.btnCtoF);
-        btnCtoF.setOnClickListener(new View.OnClickListener(){
+        btnCtoF = (Button) findViewById(R.id.btnCtoF);
+        btnCtoF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CtoF(v);
             }
         });
 
-        btnFtoC = (Button)findViewById(R.id.btnFtoC);
-        btnFtoC.setOnClickListener(new View.OnClickListener(){
+        btnFtoC = (Button) findViewById(R.id.btnFtoC);
+        btnFtoC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FtoC(v);
             }
         });
 
-        btnClear = (Button)findViewById(R.id.btnClear);
-        btnClear.setOnClickListener(new View.OnClickListener(){
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 clear(v);
@@ -42,32 +44,42 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void CtoF(View view)
-    {
-        EditText txtdoC = (EditText)findViewById(R.id.txtDoC);
+    public void CtoF(View view) {
+        EditText txtdoC = (EditText) findViewById(R.id.txtDoC);
         EditText txtdoF = findViewById(R.id.txtDoF);
-        double doC = Double.parseDouble(txtdoC.getText().toString());
-        convert c = new convert();
-        c.setDoC(doC);
-        c.converCtoF();
-        double doF = c.getDoF();
-        txtdoF.setText(String.valueOf(doF));
+        String stxtdoC = txtdoC.getText().toString();
+        if (stxtdoC.matches("")) {
+            Toast.makeText(this, "Vui lòng nhập độ C !!!", Toast.LENGTH_SHORT).show();
+        } else {
+            double doC = Double.parseDouble(txtdoC.getText().toString());
+            convert c = new convert();
+            c.setDoC(doC);
+            c.converCtoF();
+            double doF = c.getDoF();
+            txtdoF.setText(String.valueOf(doF));
+        }
     }
 
-    public void FtoC(View view)
-    {
-        EditText txtdoF = (EditText)findViewById(R.id.txtDoF);
+    public void FtoC(View view) {
+        EditText txtdoF = (EditText) findViewById(R.id.txtDoF);
         EditText txtdoC = findViewById(R.id.txtDoC);
-        double doF = Double.parseDouble(txtdoF.getText().toString());
-        convert c = new convert();
-        c.setDoF(doF);
-        c.convertFtoC();
-        double doC = c.getDoC();
-        txtdoC.setText(String.valueOf(doC));
+        String stxtdoF = txtdoF.getText().toString();
+
+        if (stxtdoF.matches("")) {
+            Toast.makeText(this, "Vui lòng nhập độ F !!!", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            double doF = Double.parseDouble(txtdoF.getText().toString());
+            convert c = new convert();
+            c.setDoF(doF);
+            c.convertFtoC();
+            double doC = c.getDoC();
+            txtdoC.setText(String.valueOf(doC));
+        }
     }
 
-    public void clear(View view)
-    {
+    public void clear(View view) {
         EditText txtdoF = findViewById(R.id.txtDoC);
         EditText txtdoC = findViewById(R.id.txtDoF);
 
